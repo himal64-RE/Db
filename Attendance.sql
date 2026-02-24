10d08a39d78fab23c24ed15205290ca83a54d281
Himal <salinadhikari1324@gmail.com>
2026-02-24 07:32:40 +0545

    Attendance

---
+++ Attendance.sql
@@ -0,0 +1,42 @@
+#Perform the following:
+#Rename the table dept as department
+
+create database Test1;
+use Test1;
+
+show databases;
+
+create table Dept(
+DEPTNO int PRIMARY KEY,
+DNAME varchar(10),
+LOC varchar(10)
+);
+
+
+
+
+#Rename table dept as department.
+
+rename table dept to department;
+
+
+alter table department
+ADD PINCODE int;
+
+#Add column PINCODE
+
+alter table department
+modify column PINCODE int not null default 0;
+
+#change DNAME to DEPT_NAME
+
+alter table department 
+change DNAME DEPT_NAME varchar(20);
+
+#Change the data type of column LOC to Char
+alter table department modify LOC Char(20);
+
+#Drop the table department
+drop table department;
+
+select * from departments; 
--- lab1tasks.sql
+++ lab1tasks.sql
@@ -1,39 +1,4 @@
-#1. Create a db named dbemp and switch to it.
-#2. Create an employee table with appropriate data types and constaints for employee details.
 
-create database dbemp;
-use dbemp;
-
-create table employee(
-EmpID int NOT NULL PRIMARY KEY,
-FirstName varchar(20),
-LastName varchar(20),
-Gender char(1),
-DateOfBirth DATE,
-Designation varchar(50),
-DepartmentName varchar(20),
-ManagerID varchar(20),
-JoinedDate DATE,
-EmpSalary decimal(10,0),
-EmpAddress varchar(200)
-
-);
-#3. Insert at least two emloyee records into the employee table
-
-insert into employee(
-EmpId,FirstName,LastName,Gender,DateOfBirth,Designation,DepartmentName,ManagerID,JoinedDate,EmpSalary,EmpAddress)
-VALUES('001','Himal','Adhikari','M','2007-06-17','Engineer','Software Engineering','0005','2022-04-01','500000','Sanothimi-Bhaktapur')
-,('002','Ram','Thapa','M','2009-06-17','Engineer','Software Engineering','0007','2022-04-05','5250000','Lokanthali-Bhaktapur');
-
-insert into employee(
-EmpId,FirstName,LastName,Gender,DateOfBirth,Designation,DepartmentName,ManagerID,JoinedDate,EmpSalary,EmpAddress)
-VALUES('003','Sita','Shrestha','F','2003-06-18','Manager','Software Engineering','0008','2021-05-12','600000','Sanepa-Lalitpur');
-
-
-#4. Update the gender of an employee whose EmployeeID is 002.
-
-update employee
-SET Gender ="F"
 where EmpID = 002;
 
 select * from employee;
@@ -70,8 +35,7 @@ select * from employee;
 select FirstName from employee where 
 EmpID in (Select ManagerID from employee); 
 
-select FirstName from employee where
-Designation = 'Manager';
+
 
 
 
@@ -80,3 +44,13 @@ Designation = 'Manager';
 
 select * from employee where
 JoinedDate=(select Max(JoinedDate) from employee);
+
+
+select * from employee
+where FirstName like 'r%';
+select * from employee
+where LastName like '%a';
+select * from employee where FirstName Like 's%n';
+
+select * from employee where EmpAddress like 'l%';
+
