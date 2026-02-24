368ab1124ca4fad4e20da80316627e2277901ade
Himal <salinadhikari1324@gmail.com>
2026-02-24 07:43:49 +0545

    Reclaim

--- lab1tasks.sql
+++ lab1tasks.sql
@@ -1,4 +1,42 @@
+#1. Create a db named dbemp and switch to it.
+#2. Create an employee table with appropriate data types and constaints for employee details.
 
+create database dbemp;
+use dbemp;
+
+create table employee(
+EmpID int NOT NULL PRIMARY KEY,
+FirstName varchar(20),
+LastName varchar(20),
+Gender char(1),
+DateOfBirth DATE,
+Designation varchar(50),
+DepartmentName varchar(20),
+ManagerID varchar(20),
+JoinedDate DATE,
+EmpSalary decimal(10,0),
+EmpAddress varchar(200)
+
+);
+#3. Insert at least two emloyee records into the employee table
+
+insert into employee(
+EmpId,FirstName,LastName,Gender,DateOfBirth,Designation,DepartmentName,ManagerID,JoinedDate,EmpSalary,EmpAddress)
+VALUES('001','Himal','Adhikari','M','2007-06-17','Engineer','Software Engineering','0005','2022-04-01','500000','Sanothimi-Bhaktapur')
+,('002','Ram','Thapa','M','2009-06-17','Engineer','Software Engineering','0007','2022-04-05','5250000','Lokanthali-Bhaktapur'),
+('003','Sita','Shrestha','M','2006-06-17','Manager','Managers','2022-02-05','6000000','Purano-Thimi-Bhaktapur');
+
+,('002','Ram','Thapa','M','2009-06-17','Engineer','Software Engineering','0007','2022-04-05','5250000','Lokanthali-Bhaktapur');
+
+insert into employee(
+EmpId,FirstName,LastName,Gender,DateOfBirth,Designation,DepartmentName,ManagerID,JoinedDate,EmpSalary,EmpAddress)
+VALUES('003','Sita','Shrestha','F','2003-06-18','Manager','Software Engineering','0008','2021-05-12','600000','Sanepa-Lalitpur');
+
+
+#4. Update the gender of an employee whose EmployeeID is 002.
+
+update employee
+SET Gender ="F"
 where EmpID = 002;
 
 select * from employee;
